63de9d10248884578e9554d0be30ba2c97f6f132
AngeloRegatieri <angelo4526@gmail.com>
2023-10-17 23:03:14 -0300

    Commit do projeto blog Angular, bootcamp santander

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "PAVÊ DE PÃO DE MEL!",
+    "description": "Segue o link para a receita! https://claudia.abril.com.br/receitas/pave-de-pao-de-mel",
+    "photoCover":"https://comidinhasdochef.com/wp-content/uploads/2017/11/04-Pav%C3%AA-de-P%C3%A3o-de-Mel.jpg"
+  },
+  {
+    "id":"2",
+    "title": "ARROZ CARRETEIRO!",
+    "description": "Segue o link para a receita! https://www.tudogostoso.com.br/receita/15762-arroz-carreteiro-simples.html",
+    "photoCover":"https://www.comidaereceitas.com.br/wp-content/uploads/2021/08/arroz_carreteiroo.jpg"
+  },
+  {
+    "id":"3",
+    "title": "STROGONOFF DE CARNE!",
+    "description": "Segue o link para a receita! https://www.receitasnestle.com.br/receitas/strogonoff-de-carne",
+    "photoCover":"https://www.receitasnestle.com.br/sites/default/files/srh_recipes/861d71239103ef70f76554abf688bfc8.jpg"
+  },
+  {
+    "id":"4",
+    "title": "LASANHA TRADICIONAL!",
+    "description": "Segue o link para a receita! https://www.tudogostoso.com.br/receita/138558-lasanha-tradicional.html",
+    "photoCover":"https://www.saboresajinomoto.com.br/uploads/images/recipes/Lasanha_tradicional.webp"
+  }
+]
